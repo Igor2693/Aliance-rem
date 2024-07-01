d50a71d165bea01fd4ea7320db85a1cdd359cecd
Igor2693 <i.bugaytsov@mail.ru>
2024-07-01 16:35:54 +0300

    fix: tg bot

--- handler.php
+++ handler.php
@@ -1,5 +1,27 @@
 <?php
-$user_name = $_POST['username'];
-$user_phone = $_POST['userphone'];
-echo "Привет, " . $user_name . "<br>";
-echo "Ваш телефон: <b>" . $user_phone . "</b>";
+$user_name = htmlspecialchars($_POST['username']);
+$user_phone = htmlspecialchars($_POST['userphone']);
+
+
+$token = "7213367169:AAEkKxmGSjjgE6Gf3swGm9Xios7cohw52YU";
+$chat_id = "-4214926859";
+
+$formData = array(
+  "Клиент: " => $user_name,
+  "Телефон: " => $user_phone
+);
+
+foreach($formData as $key => $value) {
+  $text .= $key . $value;
+}
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$text}", "r");
+
+if ($sendToTelegram) {
+  echo "Success";
+} else {
+  echo "Error";
+}
+
+
+
